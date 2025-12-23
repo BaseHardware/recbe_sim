5d7a5900a97b1f0c274fa48a07f22a0e888349fb
Hanwook Bae <qogksdnr@hotmail.com>
2025-12-23 15:37:22 +0900

    Removing redundant comments

--- src/PrimaryGeneratorAction.cc
+++ src/PrimaryGeneratorAction.cc
@@ -1,32 +1,3 @@
-//
-// ********************************************************************
-// * License and Disclaimer                                           *
-// *                                                                  *
-// * The  Geant4 software  is  copyright of the Copyright Holders  of *
-// * the Geant4 Collaboration.  It is provided  under  the terms  and *
-// * conditions of the Geant4 Software License,  included in the file *
-// * LICENSE and available at  http://cern.ch/geant4/license .  These *
-// * include a list of copyright holders.                             *
-// *                                                                  *
-// * Neither the authors of this software system, nor their employing *
-// * institutes,nor the agencies providing financial support for this *
-// * work  make  any representation or  warranty, express or implied, *
-// * regarding  this  software system or assume any liability for its *
-// * use.  Please see the license in the file  LICENSE  and URL above *
-// * for the full disclaimer and the limitation of liability.         *
-// *                                                                  *
-// * This  code  implementation is the result of  the  scientific and *
-// * technical work of the GEANT4 collaboration.                      *
-// * By using,  copying,  modifying or  distributing the software (or *
-// * any work based  on the software)  you  agree  to acknowledge its *
-// * use  in  resulting  scientific  publications,  and indicate your *
-// * acceptance of all terms of the Geant4 Software license.          *
-// ********************************************************************
-//
-//
-/// \file B4/B4a/src/PrimaryGeneratorAction.cc
-/// \brief Implementation of the B4::PrimaryGeneratorAction class
-
 #include "PrimaryGeneratorAction.hh"
 
 #include "G4Box.hh"
@@ -37,67 +8,51 @@
 #include "G4SystemOfUnits.hh"
 #include "globals.hh"
 
-namespace B4
-{
+namespace B4 {
+    PrimaryGeneratorAction::PrimaryGeneratorAction() {
+        G4int nofParticles = 1;
+        fParticleGun       = new G4ParticleGun(nofParticles);
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+        // default particle kinematic
+        //
+        auto particleDefinition = G4ParticleTable::GetParticleTable()->FindParticle("e-");
+        fParticleGun->SetParticleDefinition(particleDefinition);
+        fParticleGun->SetParticleMomentumDirection(G4ThreeVector(0., 0., 1.));
+        fParticleGun->SetParticleEnergy(300. * MeV);
+    }
 
-PrimaryGeneratorAction::PrimaryGeneratorAction()
-{
-  G4int nofParticles = 1;
-  fParticleGun = new G4ParticleGun(nofParticles);
+    PrimaryGeneratorAction::~PrimaryGeneratorAction() { delete fParticleGun; }
 
-  // default particle kinematic
-  //
-  auto particleDefinition = G4ParticleTable::GetParticleTable()->FindParticle("e-");
-  fParticleGun->SetParticleDefinition(particleDefinition);
-  fParticleGun->SetParticleMomentumDirection(G4ThreeVector(0., 0., 1.));
-  fParticleGun->SetParticleEnergy(300. * MeV);
-}
+    void PrimaryGeneratorAction::GeneratePrimaries(G4Event *event) {
+        // This function is called at the begining of event
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+        // In order to avoid dependence of PrimaryGeneratorAction
+        // on DetectorConstruction class we get world volume
+        // from G4LogicalVolumeStore
+        //
+        G4double worldZHalfLength = 0.;
+        auto worldLV              = G4LogicalVolumeStore::GetInstance()->GetVolume("World");
 
-PrimaryGeneratorAction::~PrimaryGeneratorAction()
-{
-  delete fParticleGun;
-}
+        // Check that the world volume has box shape
+        G4Box *worldBox = nullptr;
+        if (worldLV) {
+            worldBox = dynamic_cast<G4Box *>(worldLV->GetSolid());
+        }
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+        if (worldBox) {
+            worldZHalfLength = worldBox->GetZHalfLength();
+        } else {
+            G4ExceptionDescription msg;
+            msg << "World volume of box shape not found." << G4endl;
+            msg << "Perhaps you have changed geometry." << G4endl;
+            msg << "The gun will be place in the center.";
+            G4Exception("PrimaryGeneratorAction::GeneratePrimaries()", "MyCode0002", JustWarning,
+                        msg);
+        }
 
-void PrimaryGeneratorAction::GeneratePrimaries(G4Event* event)
-{
-  // This function is called at the begining of event
+        // Set gun position
+        fParticleGun->SetParticlePosition(G4ThreeVector(0., 0., -worldZHalfLength));
 
-  // In order to avoid dependence of PrimaryGeneratorAction
-  // on DetectorConstruction class we get world volume
-  // from G4LogicalVolumeStore
-  //
-  G4double worldZHalfLength = 0.;
-  auto worldLV = G4LogicalVolumeStore::GetInstance()->GetVolume("World");
-
-  // Check that the world volume has box shape
-  G4Box* worldBox = nullptr;
-  if (worldLV) {
-    worldBox = dynamic_cast<G4Box*>(worldLV->GetSolid());
-  }
-
-  if (worldBox) {
-    worldZHalfLength = worldBox->GetZHalfLength();
-  }
-  else {
-    G4ExceptionDescription msg;
-    msg << "World volume of box shape not found." << G4endl;
-    msg << "Perhaps you have changed geometry." << G4endl;
-    msg << "The gun will be place in the center.";
-    G4Exception("PrimaryGeneratorAction::GeneratePrimaries()", "MyCode0002", JustWarning, msg);
-  }
-
-  // Set gun position
-  fParticleGun->SetParticlePosition(G4ThreeVector(0., 0., -worldZHalfLength));
-
-  fParticleGun->GeneratePrimaryVertex(event);
-}
-
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
-}  // namespace B4
+        fParticleGun->GeneratePrimaryVertex(event);
+    }
+} // namespace B4
