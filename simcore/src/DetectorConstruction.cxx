e9c69e82172092d4abebfeb34bc44d6a43569c78
Hanwook Bae <qogksdnr@hotmail.com>
2025-12-24 04:47:02 +0900

    Removing the reundant DefineVolume()

--- simcore/src/DetectorConstruction.cxx
+++ simcore/src/DetectorConstruction.cxx
@@ -41,38 +41,6 @@ namespace simcore {
         G4cout << *(G4Material::GetMaterialTable()) << G4endl;
     }
 
-    G4VPhysicalVolume *DetectorConstruction::DefineVolumes() {
-        G4double worldSizeXY = 4 * m;
-        G4double worldSizeZ  = 4 * m;
-
-        // Get materials
-        G4Material *defaultMaterial = G4Material::GetMaterial("Galactic");
-
-        //
-        // World
-        //
-        G4Box *worldS = new G4Box("World",                                           // its name
-                                  worldSizeXY / 2, worldSizeXY / 2, worldSizeZ / 2); // its size
-
-        G4LogicalVolume *worldLV = new G4LogicalVolume(worldS,          // its solid
-                                                       defaultMaterial, // its material
-                                                       "World");        // its name
-
-        G4VPhysicalVolume *worldPV = new G4PVPlacement(nullptr,         // no rotation
-                                                       G4ThreeVector(), // at (0,0,0)
-                                                       worldLV,         // its logical volume
-                                                       "World",         // its name
-                                                       nullptr,         // its mother  volume
-                                                       false,           // no boolean operation
-                                                       0,               // copy number
-                                                       fCheckOverlaps); // checking overlaps
-
-        //
-        // Always return the physical World
-        //
-        return worldPV;
-    }
-
     void DetectorConstruction::ConstructSDandField() {
         // Create global magnetic field messenger.
         // Uniform magnetic field is then created automatically if
