e0568a1f4e2cad703aac0ae7a53400808d7f0130
Hanwook Bae <qogksdnr@hotmail.com>
2025-12-25 02:48:35 +0900

    Adding indexes to the Primary class

--- simobj/simobj/Primary.h
+++ simobj/simobj/Primary.h
@@ -4,28 +4,40 @@
 #include "TObject.h"
 
 #include "Math/LorentzVector.h"
-#include "Math/Vector3Dfwd.h"
 #include "Math/Vector4Dfwd.h"
 
 namespace simobj {
     class Primary : public TObject {
       public:
-        Primary() : fPDGCode(0), fPosition({0, 0, 0}), f4Momentum({0, 0, 0, 0}) {};
+        Primary()
+            : fPDGCode(0), fVertexIdx(0), fParticleIdx(0), f4Position({0, 0, 0, 0}),
+              f4Momentum({0, 0, 0, 0}) {};
         Primary(const Primary &orig);
 
         TObject *Clone(const char *) const override;
 
-        void SetXYZ(double x, double y, double z) { fPosition.SetXYZ(x, y, z); }
+        void SetXYZT(double x, double y, double z, double t) { f4Position.SetXYZT(x, y, z, t); }
         void SetPxPyPzE(double px, double py, double pz, double e) {
             f4Momentum.SetPxPyPzE(px, py, pz, e);
         }
 
-        void SetX(double a) { fPosition.SetX(a); }
-        void SetY(double a) { fPosition.SetY(a); }
-        void SetZ(double a) { fPosition.SetZ(a); }
-        double GetX() const { return fPosition.X(); }
-        double GetY() const { return fPosition.Y(); }
-        double GetZ() const { return fPosition.Z(); }
+        void SetPDGCode(int a) { fPDGCode = a; }
+        int GetPDGCode() const { return fPDGCode; }
+
+        void SetVertexIdx(int a) { fVertexIdx = a; }
+        int GetVertexIdx() const { return fVertexIdx; }
+
+        void SetParticleIdx(int a) { fParticleIdx = a; }
+        int GetParticleIdx() const { return fParticleIdx; }
+
+        void SetX(double a) { f4Position.SetPx(a); }
+        void SetY(double a) { f4Position.SetPy(a); }
+        void SetZ(double a) { f4Position.SetPz(a); }
+        void SetT(double a) { f4Position.SetE(a); }
+        double GetX() const { return f4Position.X(); }
+        double GetY() const { return f4Position.Y(); }
+        double GetZ() const { return f4Position.Z(); }
+        double GetT() const { return f4Position.T(); }
 
         void SetPx(double a) { f4Momentum.SetPx(a); }
         void SetPy(double a) { f4Momentum.SetPy(a); }
@@ -39,8 +51,8 @@ namespace simobj {
         void Print(Option_t *option = "") const override;
 
       private:
-        int fPDGCode;
-        ROOT::Math::XYZVector fPosition;
+        int fPDGCode, fVertexIdx, fParticleIdx;
+        ROOT::Math::XYZTVector f4Position;
         ROOT::Math::PxPyPzEVector f4Momentum;
 
         ClassDefOverride(simobj::Primary, 1)
--- simobj/src/Primary.cxx
+++ simobj/src/Primary.cxx
@@ -4,7 +4,8 @@
 
 namespace simobj {
     Primary::Primary(const Primary &orig)
-        : fPDGCode(0), fPosition(orig.fPosition), f4Momentum(orig.f4Momentum) {};
+        : fPDGCode(orig.fPDGCode), fVertexIdx(orig.fVertexIdx), fParticleIdx(orig.fParticleIdx),
+          f4Position(orig.f4Position), f4Momentum(orig.f4Momentum) {};
 
     TObject *Primary::Clone(const char *) const {
         return static_cast<Primary *>(new Primary(*this));
@@ -13,8 +14,9 @@ namespace simobj {
     void Primary::Print(Option_t *option) const {
         using namespace std;
         cout << "PDG code: " << fPDGCode << endl;
-        cout << "(x, y, z, global time) = " << "(" << fPosition.X() << ", " << fPosition.Y() << ", "
-             << fPosition.Z() << ") [unit: mm]" << endl;
+        cout << "Vertex-Particle index: " << fVertexIdx << "-" << fParticleIdx << endl;
+        cout << "(x, y, z, global time) = " << "(" << f4Position.X() << ", " << f4Position.Y()
+             << ", " << f4Position.Z() << ", " << f4Position.T() << ") [unit: mm, ns]" << endl;
         cout << "(px, py, pz, Ekin) = " << "(" << f4Momentum.Px() << ", " << f4Momentum.Py() << ", "
              << f4Momentum.Pz() << ", " << f4Momentum.E() << ") [unit: MeV]" << endl;
     }
